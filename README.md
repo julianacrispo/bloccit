e7747f14c45b5c00eab6b499c6f53b12fc631019
Juliana Crispo <julianacrispo@gmail.com>
2014-05-23 08:32:03 -0400

    Implemented Devise

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Bloccit: a Reddit clone to teach the fundamentals of web development and Rails 4.
 
 http://juliana-bloccit.herokuapp.com/
+
+config/application.yml stores environmental variables
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Bloccit::Application.routes.draw do
-
+  devise_for :users
+ 
   resources :posts
 
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
+
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
